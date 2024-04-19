231560bca7cc9ef2121e2de4aaab8811e814ba4a
x23213418 <157478474+x23213418@users.noreply.github.com>
2024-04-19 20:34:31 +0530

    Update python_dependencies.sh

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -3,5 +3,6 @@
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/furniture/requirements.txt
+pip install -r /home/ubuntu/furniture/boto3
 sudo chmod 777 /home/ubuntu/furniture
 sudo chmod 777 /home/ubuntu/furniture/db.sqlite3
