c62d04d0cda31bc76a59fe99ff49a8177702a923
x23213418 <157478474+x23213418@users.noreply.github.com>
2024-04-22 20:47:41 +0530

    Update settings.py

--- furniture/settings.py
+++ furniture/settings.py
@@ -145,4 +145,4 @@ AWS_S3_REGION_NAME = 'eu-west-1'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL =  None
 AWS_S3_VERIFY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
